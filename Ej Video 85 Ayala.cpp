ba0d50e39026937eeb8e4f5b30447ff438c32ae0
Ayala-22 <ayala.c.dani@gmail.com>
2026-03-17 19:22:36 -0400

    Add files via upload

---
+++ Ej Video 85 Ayala.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+	int numero[10], *dir_numeros;
+	
+	for(int i=0; i<10; i++){
+	cout<<"Digite un numero["<<i<<"]: ";
+	cin>>numero[i]; 
+	}
+	
+	dir_numeros = numero;
+	
+	for(int i=0; i<10; i++){
+		if(*dir_numeros%2==0){
+			cout<<"El Numero"<<*dir_numeros<<" es par"<<endl;
+			cout<<"Posicion. "<<dir_numeros<<endl;
+		}
+		dir_numeros++;
+	}
+	
+	getch();
+	return 0;
+}
+	
+ 
+	
+	
